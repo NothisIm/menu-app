fc752af46d85430af2b76a388e8268ab85311cc5
Anna Pekarska <nothisim@gmail.com>
2017-07-02 06:34:18 +0300

    Add semicolons

--- app/index.js
+++ app/index.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const watch = require('node-watch')
-const app = express()
-const port = 3000
+const express = require('express');
+const watch = require('node-watch');
+const app = express();
+const port = 3000;
 
-watch('./', { recursive: true}, console.log)
+watch('./', { recursive: true}, console.log);
 
-app.use(express.static('static'))
+app.use(express.static('static'));
 
 app.get('/', (request, response) => {
-	response.redirect('index.html')
-})
+    response.redirect('index.html');
+});
 
 app.listen(port, (err) => {
-	if (err) {
-		return console.log('something bad happened', err)
-	}
+    if (err) {
+        return console.log('something bad happened', err);
+    }
 
-	console.log(`server is listening on ${port}`)
-})
+    console.log(`server is listening on ${port}`);
+});
