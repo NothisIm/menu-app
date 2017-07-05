6a7e20a2ac1e57ed4109434356bf7af4ce74c330
Anna Pekarska <nothisim@gmail.com>
2017-07-05 22:18:50 +0300

    Change project structure

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const watch = require('node-watch');
+const ejs = require('ejs');
 const app = express();
 const port = 3000;
 
 watch('./', { recursive: true}, console.log);
 
-app.use(express.static('static'));
+app.use(express.static('../sources'));
+app.engine('html', ejs.renderFile);
+app.set('views', 'src/views');
 
 app.get('/', (request, response) => {
-    response.redirect('index.html');
+    response.render('index.html');
 });
 
 app.listen(port, (err) => {
